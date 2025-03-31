46fc20707fa89f26e48aee3b0704ec974c7d92fd
EXCELSIOR <excelsior@mail.ru>
2025-03-31 18:54:31 +0300

    Обработчик команды /start

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import telebot
 
 bot = telebot.TeleBot('8181700980:AAFw-EsOg3F0CUdkyVETdLS5LqKMQbTOvew')
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Привет!")
+
+
 bot.infinity_polling()
